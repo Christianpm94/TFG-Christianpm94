f9c717ad0524833e0f477e28db858de3a29a9cae
Christianpm94 <christian941129@gmail.com>
2025-05-27 20:48:09 +0200

    Create Components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CreateMatchComponent } from './pages/create-match/create-match.component';
+import { MatchDetailComponent } from './pages/match-detail/match-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-match', component: CreateMatchComponent },
+  { path: 'match/:id', component: MatchDetailComponent },
+  { path: '**', redirectTo: '' },
+];
