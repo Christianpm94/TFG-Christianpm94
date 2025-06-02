50126938138847bde247c196034c8afea7912697
Christianpm94 <christian941129@gmail.com>
2025-06-02 20:51:41 +0200

    Navbar

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: []
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  user: any = null;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    const token =this.authService.getToken();
+    if (this.authService.getToken()) {
+      this.authService.getUser().subscribe({
+        next: (data) => this.user = data,
+        error: () => this.user = null
+      });
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.user = null;
+    window.location.reload(); // recarga para actualizar visibilidad del navbar
+  }
 }
